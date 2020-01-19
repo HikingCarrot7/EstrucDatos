a75328498690a533284979ac3531f29d2c198a11
Mohammed <47760015+HikingCarrot7@users.noreply.github.com>
2020-01-19 11:10:27 -0600

    F

--- ADAS/src/aeropuerto/Esquema.java
+++ ADAS/src/aeropuerto/Esquema.java
@@ -56,14 +56,7 @@ public final class Esquema extends JPanel
         for (int i = 0, x = OFFSET_X; i < aeropuerto.vuelosDisponibles(); i++, x += FLY_TO_FLY + RECT_WIDTH)
         {
             String claveVuelo = String.valueOf(vuelos.removeFirst().getClave());
-
-            Rectangle textBounds = fm.getStringBounds(claveVuelo, g).getBounds();
-            Rectangle posicionBounds = fm.getStringBounds(String.valueOf((i + 1)), g).getBounds();
-
-            g.drawString(String.valueOf(i + 1), x + (RECT_WIDTH - posicionBounds.width) / 2, y - 1);
-            g.drawString(claveVuelo, x + (RECT_WIDTH - textBounds.width) / 2, y + (RECT_HEIGHT + fm.getAscent()) / 2);
-
-            g.drawRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+            drawVuelo(g, fm, x, y, i, claveVuelo);
 
             if (aeropuerto.vuelosDisponibles() - (i + 1) > 0)
                 if ((i + 1) % MAX_VUELOS_POR_LINEA == 0)
@@ -82,14 +75,15 @@ public final class Esquema extends JPanel
 
     }
 
-    private void drawLargeLine(Graphics2D g, int x, int y)
+    private void drawVuelo(Graphics2D g, FontMetrics fm, int x, int y, int indexVuelo, String claveVuelo)
     {
-        g.drawLine(x, y, x + FLY_TO_FLY, y);
-        g.drawLine(x + FLY_TO_FLY, y, x + FLY_TO_FLY, y + RECT_HEIGHT);
-        g.drawLine(x + FLY_TO_FLY, y + RECT_HEIGHT, OFFSET_X - OFFSET_X / 2, y + RECT_HEIGHT);
-        g.drawLine(OFFSET_X - OFFSET_X / 2, y + RECT_HEIGHT, OFFSET_X - OFFSET_X / 2, y + SEPARACION_POR_LINEA + RECT_HEIGHT);
-        g.drawLine(OFFSET_X - OFFSET_X / 2, y + SEPARACION_POR_LINEA + RECT_HEIGHT, OFFSET_X, y + SEPARACION_POR_LINEA + RECT_HEIGHT);
-        drawTiangle(g, OFFSET_X, y + SEPARACION_POR_LINEA + RECT_HEIGHT, LADO_TRIANGULO);
+        Rectangle textBounds = fm.getStringBounds(claveVuelo, g).getBounds();
+        Rectangle posicionBounds = fm.getStringBounds(String.valueOf((indexVuelo + 1)), g).getBounds();
+
+        g.drawString(String.valueOf(indexVuelo + 1), x + (RECT_WIDTH - posicionBounds.width) / 2, y - 1);
+        g.drawString(claveVuelo, x + (RECT_WIDTH - textBounds.width) / 2, y + (RECT_HEIGHT + fm.getAscent()) / 2);
+
+        g.drawRect(x, y, RECT_WIDTH, RECT_HEIGHT);
     }
 
     private void drawTiangle(Graphics2D g, int x, int y, int lenght)
@@ -115,6 +109,16 @@ public final class Esquema extends JPanel
 
         triangle.closePath();
         g.fill(triangle);
+    }
+
+    private void drawLargeLine(Graphics2D g, int x, int y)
+    {
+        g.drawLine(x, y, x + FLY_TO_FLY, y);
+        g.drawLine(x + FLY_TO_FLY, y, x + FLY_TO_FLY, y + RECT_HEIGHT);
+        g.drawLine(x + FLY_TO_FLY, y + RECT_HEIGHT, OFFSET_X - OFFSET_X / 2, y + RECT_HEIGHT);
+        g.drawLine(OFFSET_X - OFFSET_X / 2, y + RECT_HEIGHT, OFFSET_X - OFFSET_X / 2, y + SEPARACION_POR_LINEA + RECT_HEIGHT);
+        g.drawLine(OFFSET_X - OFFSET_X / 2, y + SEPARACION_POR_LINEA + RECT_HEIGHT, OFFSET_X, y + SEPARACION_POR_LINEA + RECT_HEIGHT);
+        drawTiangle(g, OFFSET_X, y + SEPARACION_POR_LINEA + RECT_HEIGHT, LADO_TRIANGULO);
     }
 
 }
