10c8667fa132a4afc09e0d96b60962d2f0ea865f
Mohammed <47760015+HikingCarrot7@users.noreply.github.com>
2020-02-29 08:59:48 -0600

    ajustes

--- Directorios/src/controller/VistaController.java
+++ Directorios/src/controller/VistaController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -83,6 +84,11 @@ public class VistaController implements Initializable
         initTabla(tablaListaEncontrada);
     }
 
+    /**
+     * Iniciamos las columnas de las tablas donde se mostrarán los directorios.
+     *
+     * @param table La {@link TableView} a inicializar.
+     */
     private void initTabla(TableView<Directorio> table)
     {
         for (int i = 0; i < table.getColumns().size(); i++)
@@ -106,25 +112,51 @@ public class VistaController implements Initializable
         }
     }
 
+    /**
+     * Método que se llama cuando presionamos el botón <em>Buscar</em>.
+     *
+     * @param e El {@link ActionEvent} que se crea al realizar la acción anterior.
+     */
     @FXML
-    private void busqueda(ActionEvent event)
+    private void busqueda(ActionEvent e)
     {
 
     }
 
+    /**
+     * Método que se llama cuando presionamos el botón <em>Buscar directorio</em>.
+     *
+     * @param e El {@link ActionEvent} que se crea al realizar la acción anterior.
+     */
     @FXML
-    private void seleccionarDirectorio(ActionEvent event)
+    private void seleccionarDirectorio(ActionEvent e)
     {
         limpiarTablas();
 
-        DirectoryChooser directorio = new DirectoryChooser();
-        File file = directorio.showDialog(null);
+        File file = obtenerRutaDirectorio();
 
         if (file != null)
         {
             entradaDirectorio.setText(file.getAbsolutePath());
+            procesarDirectorios(file);
+        }
 
-            Thread t = new Thread(() ->
+    }
+
+    /**
+     * Inicia el proceso para recuperar y ordenar todos los directorios encontrados en el {@link File} especificado.
+     *
+     * @param file El {@link File} que representa el directorio ha buscar todos los archivos.
+     */
+    private void procesarDirectorios(File file)
+    {
+        /**
+         * No debe someterse al hilo <em>JavaFx Application Thread</em> a una tarea pesada, ya que congelaríamos toda la GUI de la aplicación mientras se realiza todo el proceso.
+         */
+        Task<Void> task = new Task<Void>()
+        {
+            @Override
+            protected Void call() throws Exception
             {
                 long antes = System.currentTimeMillis();
 
@@ -132,7 +164,7 @@ public class VistaController implements Initializable
                 currentScene.setCursor(Cursor.WAIT);
                 ObservableList<Directorio> directorios = FXCollections.observableArrayList();
 
-                tableManager.rellenarTabla(directorios, tablaListaEncontrada);
+                rellenarTablaEncontrados(directorios);
                 directoryManager.obtenerTodosArchivos(directorios, file);
 
                 rellenarTablaEncontrados(directorios);
@@ -141,20 +173,48 @@ public class VistaController implements Initializable
                 currentScene.setCursor(Cursor.DEFAULT);
 
                 actualizarReloj(System.currentTimeMillis() - antes);
-            });
 
-            t.setDaemon(true);
-            t.start();
-        }
+                return null;
+            }
+
+        };
+
+        Thread t = new Thread(task);
+        t.setDaemon(true);
+        t.start();
     }
 
-    @FXML
-    private void analizarEntradaDirectorio(KeyEvent event)
+    /**
+     * Regresa la ruta del directorio seleccionado.
+     *
+     * @return El {@link File} que representa al directorio.
+     */
+    private File obtenerRutaDirectorio()
     {
-        if (event.getCode() == KeyCode.ENTER)
+        DirectoryChooser directorio = new DirectoryChooser();
+        return directorio.showDialog(null);
+    }
+
+    /**
+     * Método que se llama cuándo se presiona una tecla en el {@link TextField} para insertar la ruta del directorio.
+     *
+     * @param e El {@link KeyEvent} que se crea al realizar la acción anterior.
+     */
+    @FXML
+    private void analizarEntradaDirectorio(KeyEvent e)
+    {
+        if (e.getCode() == KeyCode.ENTER)
             System.out.println("A buscar!");
     }
 
+    /**
+     * Rellena la tabla con la lista de directorios ordenados.
+     *
+     * Primeros se ordenan los directorios según el método de ordenamiento seleccionado y luego se muestra en la tabla correspondiente.
+     *
+     * @param directorios Los directorios rescatados de la ruta insertada.
+     *
+     */
     private void rellenarTablaOrdenada(ObservableList<Directorio> directorios)
     {
         ObservableList<Directorio> direc = directorios
@@ -166,11 +226,21 @@ public class VistaController implements Initializable
         tableManager.rellenarTabla(direc, tablaListaOrdenada);
     }
 
+    /**
+     * Rellena la tabla con los registros encontrados en la ruta especificada.
+     *
+     * @param directorios Los directorios encontrados.
+     */
     private void rellenarTablaEncontrados(ObservableList<Directorio> directorios)
     {
         tableManager.rellenarTabla(directorios, tablaListaEncontrada);
     }
 
+    /**
+     * Procede a buscar cuál {@link RadioButton} está seleccionado para aplicar el método de ordenamiento adecuando.
+     *
+     * @param directorios La lista de directorios.
+     */
     public void ordenarDirectorios(ObservableList<Directorio> directorios)
     {
         if (ordenamiento.getSelectedToggle() == burbuja)
@@ -192,6 +262,11 @@ public class VistaController implements Initializable
             SorterManager.ordenarPor(SorterManager.CLAVE_MEZCLA_DIRECTA, directorios);
     }
 
+    /**
+     * Actualiza el tiempo que ha transcurrido desde que se inició el proceso de búsqueda y ordenamiento.
+     *
+     * @param tiempoTrancurrido El tiempo (en milisegundos) que ha transcurrido desde que se inició todo el proceso.
+     */
     private void actualizarReloj(long tiempoTrancurrido)
     {
         Platform.runLater(() ->
@@ -200,6 +275,9 @@ public class VistaController implements Initializable
         });
     }
 
+    /**
+     * Limpia 2 tablas que muestran los directorios.
+     */
     private void limpiarTablas()
     {
         tableManager.limpiarTabla(tablaListaEncontrada);
