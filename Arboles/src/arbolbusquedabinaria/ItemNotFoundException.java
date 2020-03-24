c0592f7abe99bdff05fe458f62c340de0f36a5b5
Mohammed <47760015+HikingCarrot7@users.noreply.github.com>
2020-03-24 08:07:06 -0600

    arbol binario

---
+++ Arboles/src/arbolbusquedabinaria/ItemNotFoundException.java
@@ -0,0 +1,20 @@
+package arbolbusquedabinaria;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class ItemNotFoundException extends RuntimeException
+{
+
+    public ItemNotFoundException()
+    {
+        this("No se encontró el elemento.");
+    }
+
+    public ItemNotFoundException(String message)
+    {
+        super(message);
+    }
+
+}
---
+++ Arboles/src/arbolbusquedabinaria/PruebaABB.java
@@ -0,0 +1,22 @@
+package arbolbusquedabinaria;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class PruebaABB
+{
+
+    public static void main(String[] args)
+    {
+        ArbolBB<Integer> arbol = new ArbolBB<>();
+
+        arbol.anadirElemento(1);
+        arbol.anadirElemento(5);
+        arbol.anadirElemento(34);
+        arbol.anadirElemento(67);
+        arbol.anadirElemento(2);
+
+    }
+
+}
