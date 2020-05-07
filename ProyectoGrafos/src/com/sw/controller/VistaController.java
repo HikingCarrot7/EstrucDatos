b0714a3c20063128005a153347dc87185862730c
Mohammed <47760015+HikingCarrot7@users.noreply.github.com>
2020-05-07 15:25:43 -0500

    vista

---
+++ ProyectoGrafos/src/com/sw/controller/VistaController.java
@@ -0,0 +1,19 @@
+package com.sw.controller;
+
+import com.sw.view.Vista;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class VistaController
+{
+
+    private final Vista vista;
+
+    public VistaController(Vista vista)
+    {
+        this.vista = vista;
+    }
+
+}
